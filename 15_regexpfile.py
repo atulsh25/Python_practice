361113675fc0b16096062ba1f937678c0565e369
Atul <gsharma.atul25@gmail.com>
2021-06-05 10:11:15 +0530

    Modified bits of regexp code

--- 15_regexpfile.py
+++ 15_regexpfile.py
@@ -4,7 +4,9 @@ numlist=list()
 for line in hand:
     line=line.rstrip()
     stuff=re.findall('X-DSAM-Confidence: ([0-9.]+)',line)
+    pos=re.search('X-DSAM-Confidence: ([0-9.]+)',line)
     if len(stuff)!=1: continue
     num=float(stuff[0])
     numlist.append(num)
+print(pos)
 print(max(numlist))
